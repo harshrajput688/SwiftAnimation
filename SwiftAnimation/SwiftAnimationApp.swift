7b6241b2a4491cf456315b8002b12320d130fa54
Harsh Rajput <77964116+harshrajput688@users.noreply.github.com>
2025-04-21 13:11:52 +0530

    Initial Commit

---
+++ SwiftAnimation/SwiftAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAnimationApp.swift
+//  SwiftAnimation
+//
+//  Created by Harsh Rajput on 21/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftAnimationTests/SwiftAnimationTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftAnimationTests.swift
+//  SwiftAnimationTests
+//
+//  Created by Harsh Rajput on 21/04/25.
+//
+
+import Testing
+
+struct SwiftAnimationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
